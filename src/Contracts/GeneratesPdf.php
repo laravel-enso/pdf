e3d77d8573e401bef716d487391cc50fbf0a51c8
mishu <mishu@xtelecom.ro>
2022-03-07 09:36:50 +0200

    adds return types

--- src/Contracts/GeneratesPdf.php
+++ src/Contracts/GeneratesPdf.php
@@ -2,7 +2,11 @@
 
 namespace LaravelEnso\Pdf\Contracts;
 
+use Illuminate\Http\Response;
+
 interface GeneratesPdf
 {
-    public function inline();
+    public function inline(): Response;
+
+    public function output(): string;
 }
